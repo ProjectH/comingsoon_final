018bcb4affcf6a2101769c5506b6eeba9712603c
Thomas Bancel <bancel.thomas@gmail.com>
2012-10-08 10:17:18 +0200

    Modified user view to show error message
    Email uniqueness is validated in the user model and shown in the view through the
    error hash. Not through a flash message
    Tests don't pass, and there is a bug in the first page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
  describe "Static pages" do
   subject{page}
+  before{visit root_path}
   
   it "should have the right links on the layout" do
-    visit root_path
+    
     #click_link "Get an invite"
     #page.should
   end
